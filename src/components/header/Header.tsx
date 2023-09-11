ff5ac6874581bc85e9cb88e739797ee92e7cc195
bao2k1qn <bao.nguyen2k1qn@gmail.com>
2023-09-11 23:27:22 +0700

    fix: change the data of the header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -9,15 +9,9 @@ const Header = () => {
         <div className="sticky top-0 w-full backdrop-blur transition-colors duration-500 z-10 border-b border-slate-900/10 dark:border-slate-50/[0.06]">
             <div className="max-w-screen-2xl mx-auto px-14 h-14 bg-white/80 backdrop-blur dark:bg-slate-900/80 flex justify-between">
                 <div className="flex items-center">
-                    <Image
-                        src="/favicon/android-chrome-192x192.png"
-                        className="h-min"
-                        width={40}
-                        height={40}
-                        alt="logo description"
-                    />
-                    <div className="text-xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 ml-4">
-                        Beauty Spa
+                    <Image src="/images/logo_RUM.png" className="h-min" width={40} height={40} alt="logo description" />
+                    <div className="text-xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-primary-500 to-pink-500 ml-4">
+                        Rum Beauty & Spa
                     </div>
                     <div className="ml-4 text-xs font-medium text-primary-600 bg-primary-400/10 rounded-full px-4 py-2 hover:bg-primary-400/20">
                         <span className="font-semibold">Slogan: </span> Looking to take your team away on a retreat
